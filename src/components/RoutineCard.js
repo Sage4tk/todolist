cf3eb929455b0a505bfc64b13053abcb3d087b72
Sage4tk <ttimbol08@gmail.com>
2021-05-01 18:00:43 +0400

    added completed button

--- src/components/RoutineCard.js
+++ src/components/RoutineCard.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 export default function RoutineCard({ content, setTodo, todo }) {
+    //remove card
     const removeCard = (itemKey) => {
         setTodo(todo.filter(({ key }) => key !== itemKey));
     }
 
+    //toggle activity
+    const toggleDone = () => {
+        let toggle = !content.completed;
+        setTodo(todo.map(item => item.key === content.key ? {...item, completed: toggle}: item))
+    }
+
     return (
         <div>
-            <p>{content.key}. {content.task}</p>
+            <p>{content.task}</p>
             <button onClick={() => removeCard(content.key)}>done</button>
+            <button onClick={toggleDone}>{content.completed ? 'true' : 'false'}</button>
         </div>
     )
 }
